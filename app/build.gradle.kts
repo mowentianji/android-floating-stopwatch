c60586abcbb622875218430051aeeb883f825ee2
root <root@LAPTOP-MGNU747A>
2026-03-14 09:55:28 +0800

    Prevent time wrap and bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.itbird.floatingstopwatch"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
